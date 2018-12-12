7a32b19421490e64754dde8719de0ef23ba48056
Oliver Lund <luol16@nti.umea>
2018-12-12 13:41:40 +0100

    Med kommentarer

--- chiffer del 2.py
+++ chiffer del 2.py
@@ -1,37 +1,41 @@
+
+# Variabler och listor
 word = ""
 key = 12
 letters = []
 crypt = []
 meny = 0
 
+
 print("<<<| Välkommen till cryptotek |>>>")
 
+# Loopar koden tills man väljer 3
 while meny != 3:
 
-    print("\n1. Kryptera: ")
+    print("\n1. Kryptera: ") # Printar ut menyn
     print("2. Dekryptera: ")
     print("3. Avsluta: ")
 
-    try:
-        meny = int(input("\n>>> Gör ett val: "))
+    try: # Gör så att programmet inte crashar om du skriver in en bokstav istället för siffra
+        meny = int(input("\n>>> Gör ett val: ")) # Sparar användarens inmatningar i variabeln meny som tillåter en att göra ett val
     except:
         print("Välj en siffra!")
 
-    if meny == 1: 
-        key = (int(input("\n>>> Skriv in en nyckel: ")))
-        word = input("\n>>> Vad vill du kryptera: ")
+    if meny == 1: #Om du trycker 1 så sker understående
+        key = (int(input("\n>>> Skriv in en nyckel: "))) # Sparar användarens inmatning i variabeln key
+        word = input("\n>>> Vad vill du kryptera: ") # Sparar användarens inmatning i listan word
 
-        for letter in word:
+        for letter in word: # För varje bokstav i listan så slår den ihop användarens inmatning i word och omvandlar det till siffror, den plusar också på med nyckeln
             letters.append(ord(letter) + key)
 
-        print(letters)
+        print(letters) # Printar ut alla bokstäver
     
-    elif meny == 2:
+    elif meny == 2: # Om du trycker 2 så sker understående
 
-        for l in letters:
-            crypt.append(chr(l -key))
-        print(crypt)
+        for l in letters: # För varje bokstav så...
+            crypt.append(chr(l -key)) # Tar den bort nyckeln och omvandlar tillbaka det krypterade till bokstäver
+        print(crypt) # Skriver ut det dekrypterade 
 
-    else:
+    else: # Helt oviktigt då det inte syns när man kör programmet i cmd
         print("<<< Det här syns inte >>>")
 
